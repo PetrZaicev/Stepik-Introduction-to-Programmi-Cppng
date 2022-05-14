2d842ba3ad9b75a73c3e8526e9639355818334fb
Petr Zaitsev <Petr_Zaicev@hotmail.com>
2022-05-14 18:13:02 +0300

    Create Task8.cpp

---
+++ 1.7 Цикл for и массивы/Task8.cpp
@@ -0,0 +1,27 @@
+/* Дан список, упорядоченный по неубыванию элементов в нем. Определите, сколько в нем различных элементов.
+Формат входных данных
+
+В первой строке вводится количество элементов в массиве. Во второй строке вводятся элементы массива.
+Формат выходных данных
+Выведите ответ на задачу. */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+  // put your code here
+  int n;  
+  cin >> n;
+  vector <int> v;  
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+    v.push_back(temp);
+  }
+  vector<int>::iterator it = std::unique (v.begin(), v.end()); 
+  v.resize( std::distance(v.begin(),it));
+  cout << v.size();  
+  return 0;
+}
